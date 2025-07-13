36613d0e0d30c66c7187b447f42826d2d86a2fcd
Nikitha N <nikitha.nrb@gmail.com>
2025-07-13 23:19:38 +0530

    Update Rcodes.R

--- Rcodes.R
+++ Rcodes.R
@@ -1,1 +1,14 @@
 
+# Addition, subtraction, multiplication, and division
+a <- 10
+b <- 5
+
+sum <- a + b
+diff <- a - b
+prod <- a * b
+quot <- a / b
+
+print(sum)
+print(diff)
+print(prod)
+print(quot)
