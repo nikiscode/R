56d7b47efa7c9126a7f6dba7c69b26977eac69ac
Nikitha N <nikitha.nrb@gmail.com>
2025-07-16 22:44:20 +0530

    Update Basics.R

--- Basics.R
+++ Basics.R
@@ -1,1 +1,23 @@
-#basics
+#basics in R
+#addition operation 
+x <- 5
+y <- 10
+z <- x + y
+print(z)
+
+#printing hello
+num <- 3.14         # Numeric
+txt <- "Hello"      # Character
+flag <- TRUE        # Logical
+
+#matrics
+m <- matrix(1:6, nrow=2)
+print(m)
+
+#ploting
+plot(c(1,2,3), c(3,2,1), type="b")
+
+#packages installation 
+install.packages("ggplot2")   # Install
+library(ggplot2)              # Load
+
